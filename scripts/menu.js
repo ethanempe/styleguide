1b0e694e4b85648233586fbfcad40edf95498576
Ethan <ethan.pitts@levatas.com>
2015-09-01 16:59:41 -0400

    ..

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,96 @@
-$(document).ready(function(){
+angular.module('StyleGuide', ['ngAnimate', 'ui.bootstrap'])
+	.controller('Header', ['$scope', function($scope) {
+		$scope.menuCollapsed = true;
 
-	$('#nav-button').click(function(){
-		$(".hamburger-close").toggleClass('open');
-		$("#fly-out-menu").toggleClass("open");
-	});
+		$scope.toggleMenu = function() {
+			$scope.menuCollapsed = !$scope.menuCollapsed;
+		}
+	}])
 
-});
+	.controller('Menu', ['$scope', function($scope) {
+		$scope.name = 'Ethan';
+		$scope.menu = [
+			{
+				title: 'Lorem ipsum dolor',
+				list:
+				[
+					'Lorem ipsum',
+					'Nisi, fugiat',
+					'Illum, vero',
+					'Id, repellat',
+					'Voluptatibus, nam',
+					'Quas, minus',
+					'Fugiat, quod',
+					'Nemo, voluptatum',
+					'Architecto, quia',
+					'Possimus, fugit'
+				]
+			},
+			{
+				title: 'Aspernatur, molestiae, rerum',
+				list:
+				[
+					'Lorem ipsum',
+					'Nisi, fugiat',
+					'Illum, vero',
+					'Id, repellat',
+					'Voluptatibus, nam',
+					'Quas, minus',
+					'Fugiat, quod',
+					'Nemo, voluptatum',
+					'Architecto, quia',
+					'Possimus, fugit'
+				]
+			},
+			{
+				title: 'Ea totam, asperiores',
+				list:
+				[
+					'Lorem ipsum',
+					'Nisi, fugiat',
+					'Illum, vero',
+					'Id, repellat',
+					'Voluptatibus, nam',
+					'Quas, minus',
+					'Fugiat, quod',
+					'Nemo, voluptatum',
+					'Architecto, quia',
+					'Possimus, fugit'
+				]
+			},
+			{
+				title: 'Cupiditate, ea, architecto',
+				list:
+				[
+					'Lorem ipsum',
+					'Nisi, fugiat',
+					'Illum, vero',
+					'Id, repellat',
+					'Voluptatibus, nam',
+					'Quas, minus',
+					'Fugiat, quod',
+					'Nemo, voluptatum',
+					'Architecto, quia',
+					'Possimus, fugit'
+				]
+			},
+			{
+				title: 'Possimus, consectetur, quia',
+				list:
+				[
+					'Lorem ipsum',
+					'Nisi, fugiat',
+					'Illum, vero',
+					'Id, repellat',
+					'Voluptatibus, nam',
+					'Quas, minus',
+					'Fugiat, quod',
+					'Nemo, voluptatum',
+					'Architecto, quia',
+					'Possimus, fugit'
+				]
+			}
+		];
+
+
+	}]);
